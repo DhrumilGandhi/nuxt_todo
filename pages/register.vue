4d95559e6461f1648f2194c7356941713d6f0acb
dhrumilgandhi <dhrumilgandhi540@gmail.com>
2025-04-04 21:55:15 +0530

    Create UI for registration page and create DB(mysql) using prisma

--- pages/register.vue
+++ pages/register.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-    <h1 class="text-3xl font-bold underline">register</h1>
+    <div class="flex bg-black h-screen">
+        <div class=" bg-zinc-900 w-[516px] p-8 flex flex-col justify-center">
+            <Logo />
+            <h1 class="text-white font-bold text-lg mt-8">Sign up for a free account</h1>
+            <p class="text-zinc-300 text-sm mt-0.5">Already registered? <span class="font-bold text-[#FFAC00] underline"> Log in </span>to your account</p>
+            <div class="mt-8">
+                <label class="text-zinc-300 font-normal block mb-0.5" for="">Email Address</label>
+                <input type="email" class="block w-full bg-[#27272A] border border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm" placeholder="you@example.com">
+            </div>
+            <div class="mt-6">
+                <label class="text-zinc-300 font-normal block mb-0.5" for="">Password</label>
+                <input type="password" class="block w-full bg-[#27272A] border border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm" placeholder="*********">
+            </div>
+            <div>
+                <button class="w-full mt-4 text-[#4B360A] bg-[#FFAC00] rounded-full px-4 py-2 text-sm font-bold flex justify-center items-center gap-2">Sign Up <ArrowRight/> </button>
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
 </template>
