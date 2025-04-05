05ca86e976276720d41dd62b61d94775dc70855f
dhrumilgandhi <dhrumilgandhi540@gmail.com>
2025-04-05 11:42:32 +0530

    Create pasword hasing and add salt

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,11 +1,16 @@
 import prisma from "~/lib/prisma";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 
 export default defineEventHandler(async (event)=>{
     const body = await readBody(event);
+    const salt = await bcrypt.genSalt(saltRounds);
+    const passwordHash = await bcrypt.hash(body.password, salt);
     await prisma.User.create({
         data: {
             email: body.email,
-            password: body.password
+            password: passwordHash,
+            salt: salt
         }
     })
     return {
