b25edcbbdb995f3ef3c9eaf2e37b015b505e9ddf
dhrumilgandhi <dhrumilgandhi540@gmail.com>
2025-04-19 10:58:14 +0530

    add user validation for fetchin notes from DB

--- server/api/notes.js
+++ server/api/notes.js
@@ -1,8 +1,27 @@
 import prisma from "~/lib/prisma";
+import jwt from 'jsonwebtoken'; 
+
 
 export default defineEventHandler(async (event) => {
     try {
-        const note = await prisma.Note.findMany();
+        const cookies = parseCookies(event);
+        const token = cookies.TodoJWT;
+
+        if(!token) {
+            throw createError({
+                statusCode: 401,
+                statusMessage: 'Not authorized to access Notes'
+            });
+        }
+
+        const decodeedToken = await jwt.verify(token, process.env.JWT_KEY);
+        const note = await prisma.Note.findMany(
+            {
+                where : {
+                    userId : decodeedToken.id
+                }
+            }
+        );
         return note;
     } catch (error) {
         console.log(error)
